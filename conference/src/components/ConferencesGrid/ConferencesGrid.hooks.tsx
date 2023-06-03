88370753948918ae273ac70a9858d666c575aa25
Andrii Aleksandrov <aleksandrovndr@gmail.com>
2023-06-04 00:15:50 +0300

    add conference removal action

--- conference/src/components/ConferencesGrid/ConferencesGrid.hooks.tsx
+++ conference/src/components/ConferencesGrid/ConferencesGrid.hooks.tsx
@@ -1,10 +1,13 @@
-import { GetConferencesData, GetConferencesResponse } from "./ConferencesGrid.types";
-import { GridRowsProp, GridColDef, GridPaginationModel } from "@mui/x-data-grid";
-import { Link } from "react-router-dom";
-import moment from "moment";
-import { useGetApi } from "../../hooks/UseGetApi";
 import { AxiosRequestConfig } from "axios";
-import { useMemo } from "react";
+import moment from "moment";
+import { useEffect, useMemo, useState } from "react";
+import { Link } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { GridActionsCellItem, GridColDef, GridPaginationModel, GridRowsProp } from "@mui/x-data-grid";
+import { useDeleteApi } from "../../hooks/useDeleteApi";
+import { useGetApi } from "../../hooks/UseGetApi";
+import { Conference } from "../../types/Conference";
+import { GetConferencesData, GetConferencesResponse } from "./ConferencesGrid.types";
 
 export const useGetConferencesApi = (paging: GridPaginationModel): GetConferencesResponse => {
   const config: AxiosRequestConfig<any> = useMemo(
@@ -17,8 +20,32 @@ export const useGetConferencesApi = (paging: GridPaginationModel): GetConference
   return useGetApi<GetConferencesData>(`/Conference`, config);
 };
 
+export const useDeleteConferenceApi = () => {
+  return useDeleteApi<{}>(`/Conference/`);
+};
+
 export const useConferencesGridProps = (conferences: GetConferencesResponse): [GridRowsProp, GridColDef[]] => {
-  const rows: GridRowsProp = conferences.data?.items ?? [];
+  const [rows, setRows] = useState<Conference[]>(conferences.data?.items ?? []);
+  const [deletedConferenceId, setDeletedConferenceId] = useState<number | null>(null);
+  const { response, performDelete: deleteConference } = useDeleteConferenceApi();
+
+  useEffect(() => {
+    if (!conferences.isLoading && !conferences.isError) {
+      setRows(conferences.data?.items ?? []);
+    }
+  }, [conferences]);
+
+  function handleDelete(conferenceId: number) {
+    setDeletedConferenceId(conferenceId);
+    deleteConference(String(conferenceId));
+  }
+  useEffect(() => {
+    if (!response.isError && !response.isLoading && deletedConferenceId) {
+      setRows((prevRows) => prevRows.filter((row) => row.id !== deletedConferenceId));
+      setDeletedConferenceId(null);
+    }
+  }, [response, deletedConferenceId]);
+
   const columns: GridColDef[] = [
     {
       headerName: "#",
@@ -45,6 +72,19 @@ export const useConferencesGridProps = (conferences: GetConferencesResponse): [G
       field: "endDate",
       minWidth: 120,
       valueFormatter: (params) => moment(params?.value).format("DD/MM/YYYY"),
+    },
+    {
+      field: "actions",
+      type: "actions",
+      width: 80,
+      flex: 1,
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon={<DeleteIcon />}
+          label="Delete Conference"
+          onClick={() => handleDelete(params.row.id)}
+        />,
+      ],
     },
   ];
 
