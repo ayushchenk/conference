c278ee27b09908976fa34268b7e6a45c8e346c7d
Andrii Aleksandrov <aleksandrovndr@gmail.com>
2023-06-03 22:16:19 +0300

    add conference update form

--- conference/src/components/ConferenceDetails/ConferenceDetails.hooks.tsx
+++ conference/src/components/ConferenceDetails/ConferenceDetails.hooks.tsx
@@ -1,7 +1,12 @@
-import { GetConferenceResponse } from "./ConferenceDetails.types";
+import { GetConferenceResponse, UpdateConferenceRequest } from "./ConferenceDetails.types";
 import { useGetApi } from "../../hooks/UseGetApi";
 import { Conference } from "../../types/Conference";
+import { usePutApi } from "../../hooks/UsePutApi";
 
 export const useGetConferenceApi = (conferenceId: number): GetConferenceResponse => {
   return useGetApi<Conference>(`/Conference/${conferenceId}`);
+};
+
+export const useUpdateConferenceApi = () => {
+  return usePutApi<UpdateConferenceRequest, Conference>(`/Conference/`);
 };
--- conference/src/components/ConferenceDetails/ConferenceDetails.types.ts
+++ conference/src/components/ConferenceDetails/ConferenceDetails.types.ts
@@ -2,3 +2,41 @@ import { Conference } from "../../types/Conference";
 import { ApiResponse } from "../../types/ApiResponse";
 
 export type GetConferenceResponse = ApiResponse<Conference>;
+
+export type UpdateConferenceRequest = {
+  id: number | null;
+  title: string;
+  acronym: string;
+  organizer: string;
+  startDate: string;
+  endDate: string;
+  keywords: string;
+  abstract: string;
+  webpage: string;
+  venue: string;
+  city: string;
+  primaryResearchArea: string;
+  secondaryResearchArea: string;
+  areaNotes: string;
+  organizerWebpage: string;
+  contactPhoneNumber: string;
+};
+
+export const initialValues: UpdateConferenceRequest = {
+  id: null,
+  title: "",
+  keywords: "",
+  abstract: "",
+  acronym: "",
+  webpage: "",
+  venue: "",
+  city: "",
+  startDate: String(new Date()),
+  endDate: String(new Date()),
+  primaryResearchArea: "",
+  secondaryResearchArea: "",
+  areaNotes: "",
+  organizer: "",
+  organizerWebpage: "",
+  contactPhoneNumber: "",
+};
