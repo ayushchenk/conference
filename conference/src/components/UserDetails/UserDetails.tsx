e9e3bef5d27bf56c93ed0a7fee1e5d1b6bae1350
Oleksandr Yushchenko <a.yushchenk@gmail.com>
2023-06-07 22:27:33 +0300

    update user details form

--- conference/src/components/UserDetails/UserDetails.tsx
+++ conference/src/components/UserDetails/UserDetails.tsx
@@ -1,7 +1,29 @@
+import { TableContainer, Paper, Table, TableBody, TableRow, TableCell, Container } from "@mui/material";
 import { UserDetailsProps } from "./UserDetails.types";
+import { FormHeader } from "../FormHeader";
 
 export const UserDetails = ({ user }: UserDetailsProps) => {
+  const tableRow = (label: string, value: string, link: boolean = false) => {
+    return (<TableRow>
+      <TableCell variant="head">{label}</TableCell>
+      <TableCell>{link ? <a href={value}>{value}</a> : <>{value}</>}</TableCell>
+    </TableRow>);
+  };
+
   return (
-    <h1>{user.fullName}</h1>
+    <Container>
+      <FormHeader>{user.fullName}</FormHeader>
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableBody>
+            {tableRow("Email", user.email)}
+            {tableRow("Country", user.country)}
+            {tableRow("Affiliation", user.affiliation)}
+            {tableRow("Webpage", user.webpage, true)}
+            {tableRow("Roles", user.roles.join(", "))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 } 
