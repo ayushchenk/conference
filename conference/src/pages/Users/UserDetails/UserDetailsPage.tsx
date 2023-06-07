080bb27bada8bc7f11a3e76063eda5f509855ba2
Oleksandr Yushchenko <a.yushchenk@gmail.com>
2023-06-08 00:26:36 +0300

    center spinner

--- conference/src/pages/Users/UserDetails/UserDetailsPage.tsx
+++ conference/src/pages/Users/UserDetails/UserDetailsPage.tsx
@@ -2,14 +2,14 @@ import { useParams } from "react-router";
 import { UserDetails } from "../../../components/UserDetails/UserDetails";
 import { useGetUserApi } from "./UserDetailsPage.hooks";
 import { UserDetailsPageProps } from "./UserDetailsPage.types";
-import { CircularProgress } from "@mui/material";
+import { LoadingSpinner } from "../../../components/LoadingSpinner";
 
 export const UserDetailsPage = ({ id }: UserDetailsPageProps) => {
   const { userId } = useParams();
   const response = useGetUserApi(id ?? Number(userId));
 
   if (response.isLoading) {
-    return <CircularProgress />;
+    return <LoadingSpinner />;
   }
 
   return (
