c82338ac859d0843a00eb4fe1c7a23aa09db0836
Oleksandr Yushchenko <a.yushchenk@gmail.com>
2023-06-08 00:16:40 +0300

    add loading spinner

--- conference/src/pages/Users/UserDetails/UserDetailsPage.tsx
+++ conference/src/pages/Users/UserDetails/UserDetailsPage.tsx
@@ -2,18 +2,19 @@ import { useParams } from "react-router";
 import { UserDetails } from "../../../components/UserDetails/UserDetails";
 import { useGetUserApi } from "./UserDetailsPage.hooks";
 import { UserDetailsPageProps } from "./UserDetailsPage.types";
+import { CircularProgress } from "@mui/material";
 
 export const UserDetailsPage = ({ id }: UserDetailsPageProps) => {
   const { userId } = useParams();
   const response = useGetUserApi(id ?? Number(userId));
 
+  if (response.isLoading) {
+    return <CircularProgress />;
+  }
+
   return (
     response.data
-      ?
-      <>
-        <UserDetails user={response.data} />
-      </>
-      :
-      <h3>User not found</h3>
+      ? <UserDetails user={response.data} />
+      : <h3>User not found</h3>
   );
 }
