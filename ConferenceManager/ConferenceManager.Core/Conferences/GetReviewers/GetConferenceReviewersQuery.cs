d63563d29270e4312c50b16a81e111aa7657e996
Oleksandr Yushchenko <a.yushchenk@gmail.com>
2023-06-21 13:51:48 +0300

    add api for conference reviewers

---
+++ ConferenceManager/ConferenceManager.Core/Conferences/GetReviewers/GetConferenceReviewersQuery.cs
@@ -0,0 +1,15 @@
+﻿using ConferenceManager.Core.Account.Common;
+using ConferenceManager.Core.Common.Queries;
+
+namespace ConferenceManager.Core.Conferences.GetReviewers
+{
+    public class GetConferenceReviewersQuery : GetEntityPageQuery<UserDto>
+    {
+        public int ConferenceId { get; }
+
+        public GetConferenceReviewersQuery(int conferenceId, int pageIndex, int pageSize) : base(pageIndex, pageSize)
+        {
+            ConferenceId = conferenceId;
+        }
+    }
+}
---
+++ ConferenceManager/ConferenceManager.Core/Conferences/GetReviewers/GetConferenceReviewersQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using ConferenceManager.Core.Account.Common;
+using ConferenceManager.Core.Common;
+using ConferenceManager.Core.Common.Interfaces;
+using ConferenceManager.Core.Common.Model;
+using ConferenceManager.Core.Common.Model.Responses;
+using ConferenceManager.Domain.Entities;
+
+namespace ConferenceManager.Core.Conferences.GetReviewers
+{
+    public class GetConferenceReviewersQueryHandler : DbContextRequestHandler<GetConferenceReviewersQuery, EntityPageResponse<UserDto>>
+    {
+        public GetConferenceReviewersQueryHandler(
+            IApplicationDbContext context,
+            ICurrentUserService currentUser,
+            IMappingHost mapper) : base(context, currentUser, mapper)
+        {
+        }
+
+        public override async Task<EntityPageResponse<UserDto>> Handle(GetConferenceReviewersQuery request, CancellationToken cancellationToken)
+        {
+            var reviewersIds = Context.UserRoles
+                .Where(r => r.ConferenceId == request.ConferenceId && r.Role.Name == ApplicationRole.Reviewer)
+                .Select(r => r.UserId);
+
+            var reviewers = Context.Users
+                .Where(u => reviewersIds.Contains(u.Id))
+                .OrderBy(u => u.Id);
+
+            var page = await PaginatedList<ApplicationUser>.CreateAsync(reviewers, request.PageIndex, request.PageSize);
+
+            return new EntityPageResponse<UserDto>()
+            {
+                Items = page.Select(Mapper.Map<ApplicationUser, UserDto>),
+                TotalCount = page.TotalCount,
+                TotalPages = page.TotalPages,
+            };
+        }
+    }
+}
