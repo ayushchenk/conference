1df79df58cf9cd2c2ecd0d4b0fd5149b159f73c0
Oleksandr Yushchenko <a.yushchenk@gmail.com>
2023-07-05 18:37:22 +0300

    add eslint

--- conference/src/pages/Conferences/UpdateSubmissionPage.tsx
+++ conference/src/pages/Conferences/UpdateSubmissionPage.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Container } from "@mui/material";
 import { CreateSubmissionForm } from "../../components/CreateSubmissionForm";
-import { FormHeader, FormNavHeader } from "../../components/FormHeader";
+import { FormNavHeader } from "../../components/FormHeader";
 import { useGetSubmissionApi } from "../../components/SubmissionDetails/SubmissionDetails.hooks";
 import { LoadingSpinner } from "../../components/LoadingSpinner";
 import { FormErrorAlert } from "../../components/FormErrorAlert";
